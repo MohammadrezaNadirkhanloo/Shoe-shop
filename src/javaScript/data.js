b9a2d84e6482e8906d3047dd1f2fe0c256100a41
khanloo <m.nadirkhanloo1380@gmail.com>
2024-07-20 10:26:01 +0330

    Feat : add item data score

--- src/javaScript/data.js
+++ src/javaScript/data.js
@@ -3,48 +3,56 @@ export const products = [
     id: 1,
     title: "Nike",
     price: 160,
-    imageUrl: "./assets/img/Frame 611.png",
+    imageUrl: "./assets/img/Frame611.png",
+    score:3
   },
   {
     id: 2,
     title: "Nike",
     price: 22.49,
-    imageUrl: "./assets/img/Frame 611 (1).png",
+    imageUrl: "./assets/img/Frame(1).png",
+    score:2
   },
   {
     id: 3,
     title: "Nike",
     price: 155.3,
     imageUrl: "./assets/img/cosmic-unity.png",
+    score:4
   },
   {
     id: 4,
     title: "Nike",
     price: 147.66,
-    imageUrl: "./assets/img/air-max-90-flyease (1).png",
+    imageUrl: "./assets/img/air-max-90-flyea.png",
+    score:5
   },
   {
     id: 5,
     title: "Nike",
     price: 220.55,
-    imageUrl: "./assets/img/Frame 611 (1).png",
+    imageUrl: "./assets/img/Frame(1).png",
+    score:3
   },
   {
     id: 6,
     title: "Nike",
     price: 778.6,
-    imageUrl: "./assets/img/Frame 611.png",
+    imageUrl: "./assets/img/air-max-90-flyea.png",
+    score:3
   },
   {
     id: 7,
     title: "Nike",
     price: 25.55,
-    imageUrl: "./assets/img/Frame 611.png",
+    imageUrl: "./assets/img/Frame611.png",
+    score:1
   },
   {
     id: 8,
     title: "Nike",
     price: 78.88,
-    imageUrl: "./assets/img/Frame 611.png",
+    imageUrl: "./assets/img/Frame(1).png",
+    score:4
   },
 ];
