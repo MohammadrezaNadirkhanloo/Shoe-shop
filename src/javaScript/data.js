88055c41e834bf2dc6607c87ead4b9e0e6e9ce67
khanloo <m.nadirkhanloo1380@gmail.com>
2024-07-20 10:01:11 +0330

    Feat : add data products

---
+++ src/javaScript/data.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    title: "Nike",
+    price: 160,
+    imageUrl: "./assets/img/Frame 611.png",
+  },
+  {
+    id: 2,
+    title: "Nike",
+    price: 22.49,
+    imageUrl: "./assets/img/Frame 611 (1).png",
+  },
+  {
+    id: 3,
+    title: "Nike",
+    price: 155.3,
+    imageUrl: "./assets/img/cosmic-unity.png",
+  },
+  {
+    id: 4,
+    title: "Nike",
+    price: 147.66,
+    imageUrl: "./assets/img/air-max-90-flyease (1).png",
+  },
+  {
+    id: 5,
+    title: "Nike",
+    price: 220.55,
+    imageUrl: "./assets/img/Frame 611 (1).png",
+  },
+  {
+    id: 6,
+    title: "Nike",
+    price: 778.6,
+    imageUrl: "./assets/img/Frame 611.png",
+  },
+  {
+    id: 7,
+    title: "Nike",
+    price: 25.55,
+    imageUrl: "./assets/img/Frame 611.png",
+  },
+  {
+    id: 8,
+    title: "Nike",
+    price: 78.88,
+    imageUrl: "./assets/img/Frame 611.png",
+  },
+];
