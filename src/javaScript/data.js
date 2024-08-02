81660a75a52fdd26ca72a51f7c4a4afe90b80a8e
khanloo <m.nadirkhanloo1380@gmail.com>
2024-08-02 11:17:42 +0330

    Feat : add color

--- src/javaScript/data.js
+++ src/javaScript/data.js
@@ -4,55 +4,63 @@ export const products = [
     title: "Nike Air Max",
     price: 160,
     imageUrl: "./assets/img/Frame611.png",
-    score:3
+    score: 3,
+    color: "White",
   },
   {
     id: 2,
     title: "Nike Alphafly",
     price: 22.49,
     imageUrl: "./assets/img/Frame(1).png",
-    score:2
+    score: 2,
+    color: "Red",
   },
   {
     id: 3,
     title: "Nike Alphafly",
     price: 155.3,
     imageUrl: "./assets/img/cosmic-unity.png",
-    score:4
+    score: 4,
+    color: "Purple",
   },
   {
     id: 4,
     title: "Nike Mercurial",
     price: 147.66,
     imageUrl: "./assets/img/air-max-90-flyea.png",
-    score:5
+    score: 5,
+    color: "Blue",
   },
   {
     id: 5,
     title: "Nike G.T",
     price: 220.55,
     imageUrl: "./assets/img/Frame(1).png",
-    score:3
+    score: 3,
+    color: "Red",
   },
   {
     id: 6,
     title: "Nike Pegasus",
     price: 778.6,
     imageUrl: "./assets/img/air-max-90-flyea.png",
-    score:3
+    score: 3,
+    color: "Blue",
   },
   {
     id: 7,
     title: "Nike Maxfly",
     price: 25.55,
     imageUrl: "./assets/img/Frame611.png",
-    score:1
+    score: 1,
+    color: "White",
   },
   {
     id: 8,
     title: "Nike Vapor",
     price: 78.88,
     imageUrl: "./assets/img/Frame(1).png",
-    score:4
+    score: 4,
+    color: "Red",
   },
 ];
